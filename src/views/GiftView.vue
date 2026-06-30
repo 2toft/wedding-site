858e82d030820185337b2a77604c221f714dfe6e
Johan Nåtoft <natoft.johan@gmail.com>
2026-06-30 23:42:07 +0200

    feat: update gift list details and styling for improved user experience

--- src/views/GiftView.vue
+++ src/views/GiftView.vue
@@ -14,16 +14,25 @@
     />
 
     <h2>{{ $t("gift.title") }}</h2>
-    <p class="intro text-body">{{ $t("gift.intro") }}</p>
-    <p class="details text-body">{{ $t("gift.details") }}</p>
-
-    <p class="gift-list-status text-body">
-      {{ $t("gift.listStatus") }}
+    <p class="intro text-body">
+      {{ $t("gift.intro") }} {{ $t("gift.details") }}
     </p>
 
-    <button type="button" class="btn" disabled>
-      {{ $t("gift.button") }}
-    </button>
+    <div class="gift-card">
+      <div class="gift-list-info text-body">
+        <p>{{ $t("gift.listInfo2") }}</p>
+        <p>{{ $t("gift.listInfo3") }}</p>
+      </div>
+
+      <a
+        class="btn gift-btn"
+        :href="$t('gift.listUrl')"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {{ $t("gift.button") }}
+      </a>
+    </div>
   </div>
 </template>
 
@@ -51,14 +60,14 @@
   }
 
   .side-art-left {
-    left: clamp(8px, 2vw, 24px);
-    top: 73%;
+    right: 100%;
+    top: 80%;
     width: clamp(104px, 18.2vw, 156px);
   }
 
   .side-art-right {
     right: clamp(8px, 2vw, 24px);
-    bottom: 14%;
+    top: 95%;
   }
 
   @include mobile {
@@ -68,28 +77,57 @@
     }
 
     .side-art-left {
-      width: clamp(64px, 17.55vw, 93px);
+      left: 5%;
+      top: 3%;
     }
+
+    .side-art-right {
+      right: clamp(8px, 2vw, 24px);
+      top: 94%;
+  }
   }
 }
 
-.intro,
-.details {
+.intro {
   margin: 0 auto $spacing-md;
   max-width: 56ch;
 }
 
-.btn {
-  margin-top: $spacing-sm;
+.gift-card {
+  background: $color-dark-green;
+  color: $color-light-green;
+  border-radius: 8px;
+  padding: $spacing-md;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.05);
+  max-width: 720px;
+  margin: 0 auto;
+}
 
-  &:disabled {
-    cursor: not-allowed;
+.gift-list-info {
+  margin: 0 auto $spacing-sm;
+  max-width: 58ch;
+  // text-align: left;
+  color: inherit;
+
+  p {
+    color: inherit;
+    margin: 0 0 $spacing-sm;
+  }
+
+  p:last-child {
+    margin-bottom: 0;
   }
 }
 
-.gift-list-status {
-  margin: 0 auto $spacing-xs;
-  max-width: 56ch;
-  text-decoration: underline;
+.gift-btn {
+  display: inline-block;
+  margin-top: $spacing-sm;
+  background-color: $color-light-green;
+  color: $color-dark-green;
+
+  &:hover {
+    background-color: $color-light-green;
+    color: $color-dark-green;
+  }
 }
 </style>
